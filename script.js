4ff1a6cd557b4f8fba14c59c7020414a372b8fbe
Mark Hallam <nippermh@hotmail.com>
2022-10-22 14:00:14 +1100

    Update script.js
    working crud

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ const url = 'http://localhost:3000/girls';
 const form = document.getElementById('addDataForm');
 const dataArea = document.getElementById('dataOutput');
 let output = '';
-const deleteBtn = document.querySelector('delete-btn');
-
+const nameValue = document.querySelector('.name');
+const btnSubmit = document.querySelector('.btn-submit');
 //Get
 fetch(url) 
   .then((resp) => resp.json())
   //.then((data) => console.log(data))
   .then(data => renderData(data));
-
+  
 const renderData = (girls) => {
   girls.forEach(girl => {
     output += `
    <div class="outer-box" data-id=${girl.id}>
-      <div class="box">${girl.name}</div>
+      <div class="box name">${girl.name}</div>
       <button class="delete-btn">Delete</button>
       <button class="edit-btn">Edit</button>
   </div>
@@ -24,7 +24,7 @@ const renderData = (girls) => {
   dataArea.innerHTML = output;
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+
 //Add a girl name
 //POST
 form.addEventListener("submit", (e) => {
@@ -45,31 +45,53 @@ form.addEventListener("submit", (e) => {
       dataArr.push(data);
       renderData(dataArr);
     })
+    //reset the input field to empty
+    girlName.value = '';
 });
 
-
+//DELETE
 dataOutput.addEventListener('click', function(event) {
   let deleteIsPressed = event.target.className === "delete-btn"
   let editIsPressed = event.target.className === "edit-btn"
+  let id = event.target.parentElement.dataset.id;
   if(deleteIsPressed) {
-    let id = event.target.parentElement.parentElement.dataset.id
-    console.log('delete is pressed')
-    fetch(URL/id, {
-      method: 'DELETE'
-    })
-    .then(response => response.json())
-    .then()
-
-
-
    
-  } else if (editIsPressed) {
-    let id = event.target.parentElement.dataset.id
-    console.log('edit is pressed')
-    
+    console.log('delete is pressed')
+    console.log(id)
+    fetch(url + '/' + id, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(res => res.json())
+    .then(() => {
+     id.remove();
+     })
+     window.location.reload();
+  } else if(editIsPressed) {
+
+    console.log('edit is pressed');
+    const parent = event.target.parentElement;
+    //capture the value
+    let girlName = parent.querySelector('.name').textContent;
+    console.log(girlName);
+
+    nameValue.value = girlName;
   }
-});
-
-
-//close of domcontentloaded
-});
+  //update the existing name
+  btnSubmit.addEventListener('click', (e) => {
+    e.preventDefault()
+    fetch(url + '/' + id, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: nameValue.value
+      })
+    })
+  .then(res => res.json())
+  .then(() => location.reload())
+  })
+})
